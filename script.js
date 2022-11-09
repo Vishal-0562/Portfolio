adba666563fc7cf0e01fc86d0fe4a40316ad6a2e
Vishal Singh <vishalsinghhoney0562@gmail.com>
2022-11-09 12:33:38 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+AOS.init();
+let typed = new Typed(".auto-input", {
+    strings: ["Web Developer","Web Designer","Coder","Video Editor"],
+    typeSpeed:150,
+    backSpeed: 100,
+    loop: true
+  })
+  
